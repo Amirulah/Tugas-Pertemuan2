c07bb084a32ff61104282b2da80046b62602cbd4
amirulah <amirulahkaharudin46@gmail.com>
2021-10-31 14:41:15 +0700

    Tugas Pertemuan 2

---
+++ Latihan.js
@@ -0,0 +1,37 @@
+// let dataarray = ["budi", "bulu tangkis", 30];
+
+// console.log (dataarray[0])
+// console.log (dataarray[1])
+// console.log (dataarray[2])
+
+// -----------------------------------
+// let arr = ['jeruk', 'mangga', 'apel'];
+
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[1]);
+// }
+
+// ====================================
+// let dataObject = {
+//     nama : "budi",
+//     alamat : "bogor",
+//     usia : "30",
+//     hoby :{
+//         hobi1 : "panahan",
+//         hobi2 : "berenang",
+//         hobi3 : "berkuda",
+//     }
+// }
+
+// console.log(dataObject.nama)
+// console.log(dataObject.hoby.hobi3)
+
+// // ===============================================
+
+
+
+
+
+
+
+
---
+++ latihanjs2.js
@@ -0,0 +1,43 @@
+// 1. string
+// a. buat variabel nama cari nama depan dan nama belakang
+// misal nama = susilo hartomo
+// nama depan = susilo
+// nama belakang = hartomo
+// b. buat variabel alamat yang ada nomornya
+// misal alamat = jln kalimantan 1 no : 71
+// no rumah : 71
+
+let fullname = "Amirulah Kaharudin";
+let alamat = "kalimantan 71";
+console.log("Nama depan = " + fullname.slice(0,8));
+console.log("nama belakang = " + fullname.slice(9,18));
+console.log("nama jalan = " + alamat.slice(0,10));
+console.log("Nomor = " + alamat.slice(11,13));
+
+// -------------------------------------------------------------
+
+//2. number
+//a. buat variabel hasil, angka pertama, angka kedua
+// --> operasikan - + / * pangkat
+// --> tukar nilai angka pertama menjadi angka kedua
+// misal a = 7, b = 8, menjadi a = 8, b - 7 tanpa boleh menulis langsung angkanya
+
+
+
+
+//3. array
+// a. buatlah array yang isinya nama nama buah sebanyak 5
+// buat variabel buah= " apel" masukan buah apel, kedalam array
+//hitung panjang arraynya
+//keluarkan nama array urutan ke 3
+
+
+
+
+//4. object
+// buatlah 2 buah object yang masing masing terdapat (nama product, harga, berat)
+// berapa jumlah harga kedua barang tersebut
+
+
+//5.conditional
+
